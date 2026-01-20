d90b32069d88d940331d81a8faa9a74ee0c346dd
sorrybubu <anshsrivastavaawork@gmail.com>
2026-01-20 22:24:34 +0530

    Update content and add images

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function check() {
   const pwd = document.getElementById("pwd").value;
   const error = document.getElementById("error");
 
-  if (pwd === "hername") {   // CHANGE THIS
+  if (pwd === "Worldsbestestgf") {   // CHANGE THIS
     window.location.href = "secret.html";
   } else {
     error.innerText = "That’s not it… but it’s okay. Try again.";
